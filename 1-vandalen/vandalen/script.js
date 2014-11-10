0c3bc9b549c5483d8b11dd00677d4209c2efacc5
Erland Jönsson <ej222ru@student.lnu.se>
2014-11-10 21:35:39 +0000

    L01.4 first part

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,36 @@
 "use strict";
 
+
 var makePerson = function(persArr){
 
+    var i=0;
+    var retObj = {minAge:200, maxAge:0,averageAge:0,names:""};
+    var nameArr = [];
+  //  var nameArr2 = ["John Häggerud", "Johan Leitet", "Mats Loock"];
+    for (i=0;i<persArr.length;i+=1){
+        var ageDate = new Date(persArr[i].born.substr(0,4),persArr[i].born.substr(5,2),persArr[i].born.substr(8,2));
+        var today = new Date();
+        var age = today.getFullYear() - ageDate.getFullYear();
+       
+        if (retObj.minAge > age)
+        {
+            retObj.minAge = age;
+        }
+        if (retObj.maxAge < age)
+        {
+            retObj.maxAge = age;
+        }
+        retObj.averageAge += age;
+        nameArr[i] = persArr[i].name;
+    }
+    retObj.averageAge = Math.round(retObj.averageAge / persArr.length);
+   
+    nameArr.sort(String.localeCompare); // Funkar för Å,Ä,Ö på FF men inte WebKit
+    var arr = nameArr.map(function(name){return " " + name;});
+    retObj.names = arr.toString();
+    retObj.names = retObj.names.trim();
 
-	// Din kod här...
-
+//    alert(retObj.names[0]); 
+    return retObj;
 }
 
