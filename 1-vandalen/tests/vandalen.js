6bb8ea96b74c6ecb3be796cd9da3915fe34c4601
Erland Jönsson <ej222ru@student.lnu.se>
2014-11-10 22:04:47 +0000

    L01.4 fattar nu att det var medvetet att anropa med born istället för age och att man inte testades map ålder i de testfallen. Använder nu age istället för born alltså

--- 1-vandalen/tests/vandalen.js
+++ 1-vandalen/tests/vandalen.js
@@ -24,19 +24,19 @@ test( "Namnhantering (names)", function() {
 test( "Åldershantering (maxAge, minAge, aveargeAge)", function() {
 
   var indata2 = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
-  var indata = [{name: "John Häggerud", age: 38}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 47}];
+  var indata = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
   
-  equal( makePerson(indata2).maxAge, 47, "maxAge är 47" );
-  equal( makePerson(indata2).minAge, 36, "minAge är 36" );
-  equal( makePerson(indata2).averageAge, 40, "averageAge är 40" );	
+  equal( makePerson(indata).maxAge, 46, "maxAge är 46" );
+  equal( makePerson(indata).minAge, 36, "minAge är 36" );
+  equal( makePerson(indata).averageAge, 40, "averageAge är 40" );	
   
 });
 
 test( "Uppgiften totalt", function() {
 
   var indata2 = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
-  var indata = [{name: "John Häggerud", age: 38}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 47}];
+  var indata = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
   
-  deepEqual( makePerson(indata2), {minAge: 36, maxAge: 47, averageAge: 40, names: "Johan Leitet, John Häggerud, Mats Loock"}, "Uppgiften avklarad på Nivå 1" );
-  deepEqual( makePerson(indata2), {minAge: 36, maxAge: 47, averageAge: 40, names: "Johan Leitet, John Häggerud, Mats Loock"}, "Uppgiften avklarad på Nivå 2" );
+  deepEqual( makePerson(indata), {minAge: 36, maxAge: 46, averageAge: 40, names: "Johan Leitet, John Häggerud, Mats Loock"}, "Uppgiften avklarad på Nivå 1" );
+  deepEqual( makePerson(indata), {minAge: 36, maxAge: 46, averageAge: 40, names: "Johan Leitet, John Häggerud, Mats Loock"}, "Uppgiften avklarad på Nivå 2" );
 });
--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -6,23 +6,21 @@ var makePerson = function(persArr){
     var i=0;
     var retObj = {minAge:200, maxAge:0,averageAge:0,names:""};
     var nameArr = [];
-  //  var nameArr2 = ["John Häggerud", "Johan Leitet", "Mats Loock"];
+
     for (i=0;i<persArr.length;i+=1){
-        var ageDate = new Date(persArr[i].born.substr(0,4),persArr[i].born.substr(5,2),persArr[i].born.substr(8,2));
-        var today = new Date();
-        var age = today.getFullYear() - ageDate.getFullYear();
-       
-        if (retObj.minAge > age)
+
+        if (retObj.minAge > persArr[i].age)
         {
-            retObj.minAge = age;
+            retObj.minAge = persArr[i].age;
         }
-        if (retObj.maxAge < age)
+        if (retObj.maxAge < persArr[i].age)
         {
-            retObj.maxAge = age;
+            retObj.maxAge = persArr[i].age;
         }
-        retObj.averageAge += age;
+        retObj.averageAge += persArr[i].age;
         nameArr[i] = persArr[i].name;
     }
+
     retObj.averageAge = Math.round(retObj.averageAge / persArr.length);
    
     nameArr.sort(String.localeCompare); // Funkar för Å,Ä,Ö på FF men inte WebKit
@@ -30,7 +28,6 @@ var makePerson = function(persArr){
     retObj.names = arr.toString();
     retObj.names = retObj.names.trim();
 
-//    alert(retObj.names[0]); 
     return retObj;
 }
 
