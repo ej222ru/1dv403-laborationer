bd55c1b341eb66252110fb4cae51e77c38509f78
Erland Jönsson <ej222ru@student.lnu.se>
2014-11-22 16:41:17 +0000

    Lab 2 Moment 3

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -1,19 +1,37 @@
 "use strict";
 
-var myMessage = {
+var MessageBoard = {
+    
+    messages : [],
+    
     init:function(){
-        var mess = new Message("Min text",new Date());
+        
+        var mess1 = new Message("Min text 1",new Date());
+        var mess2 = new Message("Min text 2",new Date());
+        var mess3 = new Message("Min text 3",new Date());
+    
+        MessageBoard.messages.push(mess1);
+        MessageBoard.messages.push(mess2);
+        MessageBoard.messages.push(mess3);
+        
+        for (var i=0;i<MessageBoard.messages.length; i+=1)
+        {
+            alert(MessageBoard.messages[i] );
+
+        }
+    /*    
         alert(mess);
         alert(mess.getText());
         mess.setText("Ny text \n hej å hå \n vad nurå");
         alert(mess);
         alert(mess.getDate());
         alert(mess.getHTMLText());
-        
+    */
+    
         
         
     }
 }; 
 
-window.onload = myMessage.init;
+window.onload = MessageBoard.init;
 
