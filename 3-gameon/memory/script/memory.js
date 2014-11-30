db6b7bb6086f0c21fc2a10e8d3c80f09fe027ca5
Erland Jönsson <ej222ru@student.lnu.se>
2014-11-30 18:39:17 +0000

    Lab 3

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,15 +2,56 @@
 
 var MemoryGame = {
     pictures : [],
-    
-    turnCard: function(index){
-        var count = document.getElementById("msgCount");
-        console.log(index);
+    clicks: 0,
+    flipped: 0,
+    turnBackAtTimeoutNode1: 0,
+    turnBackAtTimeoutNode2: 0,
+
+    flippedCardsTimeout: function(){
+        if (MemoryGame.flipped === 2){
+            MemoryGame.turnBackAtTimeoutNode1.setAttribute("src", "memory/pics/0.png"); 
+            MemoryGame.turnBackAtTimeoutNode2.setAttribute("src", "memory/pics/0.png"); 
+            MemoryGame.flipped = 0;
+        }
+    },
+    turnCard: function(e, index){
+        var secondSameIndex = false;
+        if (this.flipped === 0){
+            this.turnBackAtTimeoutNode1 = e.target;
+        }
+        else if (this.flipped === 1){
+            if (this.turnBackAtTimeoutNode1 !== e.target){
+                this.turnBackAtTimeoutNode2 = e.target;
+            }
+            else{
+                secondSameIndex = true;
+            }
+        }
+
+        if (!secondSameIndex){       
+            if (this.flipped < 2){
+                this.flipped +=1;
+                e.target.parentNode.parentNode.getAttribute("data-cardID");
+                e.target.setAttribute("data-cardID", index);
+                var str = "memory/pics/" + this.pictures[index] + ".png";
+                e.target.setAttribute("src", str);          
+            };
+     
+            if (this.flipped === 2){
+                if (this.pictures[index] === this.pictures[this.turnBackAtTimeoutNode1.getAttribute("data-cardID")]){
+                    this.flipped = 0;
+                }
+                else{
+                    // start 1 sec timer, flip back turned cards at timeout
+                    setTimeout(MemoryGame.flippedCardsTimeout, 1000);
+                }
+            }
+        }        
     },
     
     
     
-    renderMemeoryTable: function(rows, cols){
+    renderMemoryTable: function(rows, cols){
         
 
         var i = 0;
@@ -35,18 +76,22 @@ var MemoryGame = {
             memoryTableBody.appendChild(memoryTableRow); 
             for (j=0;j<cols;j+=1){
                 memoryTableCell = document.createElement("td");
-                memoryTableCell.setAttribute("data-cardID", i*cols+j+1);        
+                memoryTableCell.setAttribute("data-cardID", i*cols+j);        
+
 
                 picLink = document.createElement("a");
+                picLink.setAttribute("title", "Card");
+                picLink.setAttribute("href", "#");               
+
+
                 memoryPic = document.createElement("img");
                 memoryPic.classList.add("imgCard");                
                 memoryPic.setAttribute("src", "memory/pics/0.png");
+                memoryPic.classList.add("card");
                 
                 memoryTableCell.appendChild(picLink); 
                 picLink.appendChild(memoryPic); 
                 
-                picLink.setAttribute("title", "Card");
-                picLink.setAttribute("href", "#");               
 
                 memoryTableRow.appendChild(memoryTableCell); 
             }
@@ -60,16 +105,18 @@ var MemoryGame = {
         MemoryGame.pictures = RandomGenerator.getPictureArray(rows, columns);
         console.log(MemoryGame.pictures);
         
-        var card = document.getElementById("container");
-        card.onclick = function(e){
+        var node = document.getElementById("container");
+        node.onclick = function(e){
             var index;
             if (e.target.parentNode.getAttribute("title") === "Card"){
                 index = e.target.parentNode.parentNode.getAttribute("data-cardID");
-                MemoryGame.turnCard(index);
+                if (MemoryGame.flipped < 2){
+                    MemoryGame.turnCard(e, index);
+                }
             }
         };          
         
-        var test = MemoryGame.renderMemeoryTable(rows, columns);
+        MemoryGame.renderMemoryTable(rows, columns);
 
     }
 }
