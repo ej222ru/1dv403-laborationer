26b25870a05d3ae0dd83b6c7e942732860682d31
Erland Jönsson <ej222ru@student.lnu.se>
2014-11-30 12:40:23 +0000

    Lab 3

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,10 +1,61 @@
 "use strict";
 
 var MemoryGame = {
- 
+    pictures : [],
+    
+    renderMemeoryTable: function(rows, cols){
+        
+
+        var i = 0;
+        var j = 0;
+        
+        var container = document.getElementById("container"),
+            memoryBoard = document.createElement("div"),
+            memoryTable = document.createElement("table"),
+            memoryTableBody =  document.createElement("tbody"),
+            memoryTableRow,
+            memoryTableCell,
+            memoryPic,
+            picLink;
+            memoryBoard.classList.add("memoryBoard");
+            memoryTable.classList.add("memoryTable");
+            
+        container.appendChild(memoryBoard); 
+        memoryBoard.appendChild(memoryTable); 
+        memoryTable.appendChild(memoryTableBody); 
+        for (i=0;i<rows;i+=1){
+            memoryTableRow = document.createElement("tr");                    
+            memoryTableBody.appendChild(memoryTableRow); 
+            for (j=0;j<cols;j+=1){
+                memoryTableCell = document.createElement("td");
+
+                picLink = document.createElement("a"),
+                memoryPic = document.createElement("img"),            
+                memoryTableCell.appendChild(picLink); 
+                picLink.appendChild(memoryPic); 
+                
+               memoryPic.setAttribute("src", "../pics/1.png");
+                picLink.setAttribute("title", "Card");
+                picLink.setAttribute("href", "#");               
+
+
+
+                memoryTableRow.appendChild(memoryTableCell); 
+            }
+        }
+    },
     init:function(){
         
-        alert("hej");
+        var rows = 4;
+        var columns = 4;
+        
+        MemoryGame.pictures = RandomGenerator.getPictureArray(rows, columns);
+        console.log(MemoryGame.pictures);
+        
+        
+        
+        var test = MemoryGame.renderMemeoryTable(rows, columns);
+
     }
 }
 
