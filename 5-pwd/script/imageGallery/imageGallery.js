7b1c3f572e327cb29238dcadf1d7c14016347eb8
Erland Jönsson <ej222ru@student.lnu.se>
2015-01-04 14:00:11 +0000

    imageGallery before size change

--- 5-pwd/script/imageGallery/imageGallery.js
+++ 5-pwd/script/imageGallery/imageGallery.js
@@ -38,9 +38,7 @@ function ImageGallery(_instance) {
     };            
     this.addThumbs = function(_thumbObjArray, _instance){
         var windowInstance = document.getElementById("WindowMain"+that.instanceId);
-        var index = 0;
-        console.log(_thumbObjArray[0].thumbURL);
-        
+
 /*        
         _thumbObjArray[index].thumbURL
         _thumbObjArray[index].thumbWidth
@@ -49,62 +47,43 @@ function ImageGallery(_instance) {
         _thumbObjArray[index].Width
         _thumbObjArray[index].Height
 */
-
+        var newThumb = document.createElement("div");
+        newThumb.classList.add("imgGallery");
+    
+        windowInstance.appendChild(newThumb);
         
-            var newThumb = document.createElement("div");
-            newThumb.classList.add("imgGallery");
-            newThumb.setAttribute("title", "thumbPicImg");        
-            newThumb.setAttribute("ThumbId", index);        
-            windowInstance.appendChild(newThumb);
+        var index = 0;
+        for (index; index < _thumbObjArray.length; index+=1) {
+
+
             
             var newThumbLink = document.createElement("a");
             newThumbLink.classList.add("thumbPicLink");
             newThumbLink.setAttribute("href", "#");
     
+//            windowInstance.appendChild(newThumbLink);
             newThumb.appendChild(newThumbLink);
     
             var newThumbImg = document.createElement("img");
             newThumbImg.classList.add("thumbPicImg");
             newThumbImg.setAttribute("title","thumbPicImg");
+            newThumbImg.setAttribute("ThumbId", index);                
             newThumbImg.setAttribute("src", _thumbObjArray[index].thumbURL);
             
             newThumbLink.appendChild(newThumbImg);
-    
+        
             newThumbImg.onclick = function(e){
-     //        newThumbLink.onclick = function(e){
                 var index;
                 if (e.target.getAttribute("title") === "thumbPicImg"){
-                    index = e.target.parentNode.parentNode.getAttribute("ThumbId");
+                    index = e.target.getAttribute("ThumbId");
 
 
                     var windowInstance = new ImageGallery(++Projekt.instanceId);  
                     var imageWindow = document.getElementById("Window"+Projekt.instanceId);                    
                     var imageWindowMain = document.getElementById("WindowMain"+Projekt.instanceId);                    
-//                    var imageWindow = document.createElement("div"); 
-//                    imageWindow.setAttribute("id", "PicWindow"+index);
-//                    imageWindow.classList.add("FullSizePicture");    
-//                    imageWindow.setAttribute("title", "PicWindow");
                     imageWindow.style.height = (_thumbObjArray[index].height+52)+"px";
                     imageWindow.style.width = (_thumbObjArray[index].width+8)+"px";
-//                    imageWindowMain.style.height = (_thumbObjArray[index].height+80)+"px";
-//                    imageWindowMain.style.width = (_thumbObjArray[index].width+8)+"px";
-                    
-//                    imageWindow.style.zIndex = ++Projekt.zIndex;     
-/*                    
-                    var topLabel = document.createElement("div"); 
-                    topLabel.classList.add("topLabel");    
- 
-                    var remLink = document.createElement("a");
-                    var remPic = document.createElement("img");    
-                    remPic.classList.add("imgClose");
-                    remPic.setAttribute("src", "css/pics/remove_16.png");    
-                    remLink.setAttribute("title", "Close");
-                    remLink.setAttribute("href", "#");
-                    remLink.appendChild(remPic);    
-                    topLabel.appendChild(remLink);                     
-                    imageWindow.appendChild(topLabel);                  
-*/                    
-                    
+               
                     var imageLink = document.createElement("a");
                     imageLink.classList.add("imageLink");
                     imageLink.setAttribute("href", "#");
@@ -115,13 +94,10 @@ function ImageGallery(_instance) {
                     image.setAttribute("title","image");
                     image.setAttribute("src", _thumbObjArray[index].URL);
                     imageLink.appendChild(image);                    
-                    
-                    
-
                 }
             };  
         };    
-       
+    };   
 };
        
 ImageGallery.prototype = new Window();
