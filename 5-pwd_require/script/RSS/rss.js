66332dbc37be99e715ee3b8e485139a06f03cfb5
Erland Jönsson <ej222ru@student.lnu.se>
2015-01-09 15:17:21 +0000

    snyggar till koden

--- 5-pwd_require/script/RSS/rss.js
+++ 5-pwd_require/script/RSS/rss.js
@@ -1,60 +1,60 @@
 "use strict";
 define(["Window"], function(Window){
-var RSSWindow = function (_instance) {
-
-    Window.call(this,"css/pics/feed.png",  "RSS", _instance);
-    this.initiateWindow();
-    var that = this;
-    this.nextURL = "http://homepage.lnu.se/staff/tstjo/labbyServer/rssproxy/?url="+escape("http://www.dn.se/m/rss/senaste-nytt");        
-    this.xhr = new XMLHttpRequest();
-
-    this.getTime = function (){   
-        var date = new Date(),
-            hours = date.getHours(),
-            minutes = date.getMinutes(),
-            seconds = date.getSeconds();
-            
-        if (hours < 10){
-            hours = "0"+ hours;
-        }
-        if (minutes < 10){
-            minutes = "0"+ minutes;
-        }
-        if (seconds < 10){
-            seconds = "0"+ seconds;
-        }
-        return hours + ":" + minutes + ":" + seconds;
-     
-    }
-
-    // After timeout two unmatching cards, show backside again
-    this.updateRSS = function (){
-        that.xhr.open("GET", that.nextURL, true);
-        that.xhr.send(null);
-    }; 
+    var RSSWindow = function (_instance) {
+        var that = this;
         
-    this.start = function(){
+        Window.call(this,"css/pics/feed.png",  "RSS", _instance);
+        this.initiateWindow();              // Create common window nodes in the DOM
 
-        this.xhr.onreadystatechange = function(){
-            if ((that.xhr.readyState === 4) && (that.xhr.status === 200)) {
-                var windowMainInstance = document.getElementById(that.windowMainId);
-                windowMainInstance.innerHTML = that.xhr.responseText;  
-                var loadIcon = document.getElementById("loadIcon"+that.instanceId);
-                loadIcon.style.visibility = 'hidden';
-                document.getElementById("bottomLabelText"+that.instanceId).innerHTML = "  Senast uppdaterad " + that.getTime();                
-                setTimeout(that.updateRSS, 10000);                   
-            }
-        };
-        
-        this.updateRSS();
-        // start 10 sec timer to update
-        setTimeout(this.updateRSS, 10000);
-        
-    };    
-}
-           
-return RSSWindow;
-RSSWindow.prototype = new Window.Window();
-
+        this.nextURL    = "http://homepage.lnu.se/staff/tstjo/labbyServer/rssproxy/?url="+escape("http://www.dn.se/m/rss/senaste-nytt");        
+        this.xhr        = new XMLHttpRequest();
     
+        this.getTime    = function (){   
+            var date = new Date(),
+                hours = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds();
+                
+            if (hours < 10){
+                hours = "0"+ hours;
+            }
+            if (minutes < 10){
+                minutes = "0"+ minutes;
+            }
+            if (seconds < 10){
+                seconds = "0"+ seconds;
+            }
+            return hours + ":" + minutes + ":" + seconds;
+         
+        }
+    
+        // After timeout two unmatching cards, show backside again
+        this.updateRSS  = function (){
+            that.xhr.open("GET", that.nextURL, true);
+            that.xhr.send(null);
+        }; 
+            
+        this.start      = function(){
+    
+            this.xhr.onreadystatechange = function(){
+                if ((that.xhr.readyState === 4) && (that.xhr.status === 200)) {
+                    var windowMainInstance = document.getElementById(that.windowMainId);
+                    windowMainInstance.innerHTML = that.xhr.responseText;  
+                    var loadIcon = document.getElementById("loadIcon"+that.instanceId);
+                    loadIcon.style.visibility = 'hidden';
+                    document.getElementById("bottomLabelText"+that.instanceId).innerHTML = "  Senast uppdaterad " + that.getTime();                
+                    setTimeout(that.updateRSS, 10000);                   
+                }
+            };
+            
+            this.updateRSS();
+            // start 10 sec timer to update
+            setTimeout(this.updateRSS, 10000);
+            
+        };    
+    }
+               
+    return RSSWindow;
+    RSSWindow.prototype = new Window.Window();
+
 });
--- 5-pwd_require/script/Window.js
+++ 5-pwd_require/script/Window.js
@@ -1,141 +1,144 @@
 "use strict";
 define(["projekt", "start"], function(projekt, start){
-var Window = function(_iconURL, _name, _instance) {
-    var Projekt = require("projekt");
-    this.instanceId = _instance;
-
-    this.windowId = "Window"+_instance;   
-    this.windowMainId = "WindowMain"+_instance;  
-
-    this.calculateWindowPosition = function(_instance) {
-        
-        var posContent = this.getObjectPosition(document.getElementById("content"));
-        var newWindow = document.getElementById(this.windowId);        
-        newWindow.style.left = posContent.xl + (Projekt.newStartPositions * 300)+(++Projekt.xl)*20 + 'px';
-        newWindow.style.top = posContent.yt + (++Projekt.yt*20) + 'px';          
-
-        var posWindow = this.getObjectPosition(newWindow);
-        if ((posWindow.xl < posContent.xl) ||
-            (posWindow.xr > posContent.xr) || 
-            (posWindow.yt < posContent.yt) ||
-            (posWindow.yb > posContent.yb)){
+    var Window = function(_iconURL, _name, _instance) {
+        var Projekt         = require("projekt");
+        this.instanceId     = _instance;
+        this.windowId       = "Window"+_instance;   
+        this.windowMainId   = "WindowMain"+_instance;  
+    
+        this.calculateWindowPosition    = function(_instance) {
             
-            this.startNewPosition(document.getElementById(this.windowId));
-        }        
-    };
+            var posContent = this.getObjectPosition(document.getElementById("content"));
+            var newWindow = document.getElementById(this.windowId);        
+            newWindow.style.left = posContent.xl + (Projekt.newStartPositions * 300)+(++Projekt.xl)*20 + 'px';
+            newWindow.style.top = posContent.yt + (++Projekt.yt*20) + 'px';          
     
-    this.startNewPosition = function(newWindow){
-        Projekt.newStartPositions +=1;
-        Projekt.yt = 0;
-        Projekt.xl = 0;
-        
-        
-        var posContent = this.getObjectPosition(document.getElementById("content"));
-      
-        newWindow.style.left = (posContent.xl + (Projekt.newStartPositions * 300) + 20) + 'px';
-        newWindow.style.top = (posContent.yt + (++Projekt.yt*20)) + 'px';    
-
-        var posWindow = this.getObjectPosition(newWindow);        
-        if ((posWindow.xl < posContent.xl) ||
-            (posWindow.xr > posContent.xr) || 
-            (posWindow.yt < posContent.yt) ||
-            (posWindow.yb > posContent.yb)){
+            var posWindow = this.getObjectPosition(newWindow);
+            if ((posWindow.xl < posContent.xl) ||
+                (posWindow.xr > posContent.xr) || 
+                (posWindow.yt < posContent.yt) ||
+                (posWindow.yb > posContent.yb)){
                 
-                Projekt.newStartPositions = 0;                
-                newWindow.style.left = (posContent.xl + (Projekt.newStartPositions * 300) + 60) + 'px';    
-                newWindow.style.top = (posContent.yt + (++Projekt.yt*30)) + 'px';    
-        }        
-    }; 
-    
-    this.getObjectPosition = function (object) {
-
-        return {
-          xl : object.offsetLeft,
-          xr : object.offsetLeft + object.offsetWidth,
-          yt : object.offsetTop,
-          yb : object.offsetTop + object.offsetHeight
+                this.startNewPosition(document.getElementById(this.windowId));
+            }        
         };
-    };    
-    // Creata nodes in the DOM. By encapsulating they dont run at load  
-    this.initiateWindow = function(){  
-
-        var windowInst = document.createElement("div"); 
-        windowInst.setAttribute("id", this.windowId);
-        windowInst.classList.add("Window");    
-        windowInst.setAttribute("title", "Window");
-        windowInst.style.zIndex = Projekt.zIndex;    
-        document.getElementById("content").appendChild(windowInst);
-    
-        var topLabel = document.createElement("div"); 
-        topLabel.classList.add("topLabel");    
-        windowInst.appendChild(topLabel);
-    
-        var icon = document.createElement("img");    
-        icon.classList.add("labelIcon");
-        icon.setAttribute("src", _iconURL);    
-        topLabel.appendChild(icon);     
         
-        var name = document.createElement("div");
-        name.classList.add("appName");    
-        name.innerHTML = _name;
-        topLabel.appendChild(name);     
+        this.startNewPosition           = function(newWindow){
+            Projekt.newStartPositions +=1;
+            Projekt.yt = 0;
+            Projekt.xl = 0;
+            
+            
+            var posContent = this.getObjectPosition(document.getElementById("content"));
+          
+            newWindow.style.left = (posContent.xl + (Projekt.newStartPositions * 300) + 20) + 'px';
+            newWindow.style.top = (posContent.yt + (++Projekt.yt*20)) + 'px';    
     
-        var remLink = document.createElement("a");
-        var remPic = document.createElement("img");    
-        remPic.classList.add("imgClose");
-        remPic.setAttribute("src", "css/pics/remove_16.png");    
-        remLink.setAttribute("title", "Close");
-        remLink.setAttribute("href", "#");
-        remLink.appendChild(remPic);    
-        topLabel.appendChild(remLink);     
-        
-        var windowMain = document.createElement("div"); 
-        windowMain.classList.add("windowMain");    
-        windowMain.setAttribute("title", "Main");
-        windowMain.setAttribute("id", this.windowMainId);
-        windowInst.appendChild(windowMain);
-    
-        
-        var bottomLabel = document.createElement("div"); 
-        bottomLabel.classList.add("bottomLabel");    
-        bottomLabel.setAttribute("title", "Status");
-    
-        var loadIcon = document.createElement("img");    
-        loadIcon.classList.add("loadIcon");
-        loadIcon.setAttribute("id","loadIcon"+_instance);    
-        loadIcon.setAttribute("src", "css/pics/ajax-loader.gif");
-        bottomLabel.appendChild(loadIcon);     
-        var bottomLabelText = document.createElement("div"); 
-        bottomLabelText.setAttribute("id","bottomLabelText"+_instance);   
-        bottomLabelText.setAttribute("title", "StatusText"); 
-        bottomLabelText.style.color = "White";
-        bottomLabelText.innerHTML = "Laddar, vänta...";
-        bottomLabel.appendChild(bottomLabelText);  
-        windowInst.appendChild(bottomLabel);
-        
-        this.calculateWindowPosition(this.instanceId); 
-      
-        this.focusOrRemoveWindow = function(e){
-            // focus Window
-            var Window = null; 
-            if (e.target.getAttribute("title") === "Window")
-                Window = e.target;
-            if (e.target.parentNode.getAttribute("title") === "Window")
-                Window = e.target.parentNode;
-            if (e.target.parentNode.parentNode.getAttribute("title") === "Window")
-                Window = e.target.parentNode.parentNode;
-
-            if (Window){            
-                document.getElementById(Window.getAttribute("id")).style.zIndex = ++Projekt.zIndex;            
-            }
-            // remove Window 
-            if (e.target.parentNode.getAttribute("title") === "Close"){            
-                var node = e.target.parentNode.parentNode.parentNode;  
-                node.parentNode.removeChild (node);
-            };
+            var posWindow = this.getObjectPosition(newWindow);        
+            if ((posWindow.xl < posContent.xl) ||
+                (posWindow.xr > posContent.xr) || 
+                (posWindow.yt < posContent.yt) ||
+                (posWindow.yb > posContent.yb)){
+                    
+                    Projekt.newStartPositions = 0;                
+                    newWindow.style.left = (posContent.xl + (Projekt.newStartPositions * 300) + 60) + 'px';    
+                    newWindow.style.top = (posContent.yt + (++Projekt.yt*30)) + 'px';    
+            }        
         }; 
-        windowInst.addEventListener("click", this.focusOrRemoveWindow);          
+        
+        this.getObjectPosition          = function (object) {
+    
+            return {
+              xl : object.offsetLeft,
+              xr : object.offsetLeft + object.offsetWidth,
+              yt : object.offsetTop,
+              yb : object.offsetTop + object.offsetHeight
+            };
+        };   
+        
+        // Creata nodes in the DOM. By encapsulating they dont run at load  
+        this.initiateWindow             = function(){  
+    
+            var windowInst = document.createElement("div"), 
+                topLabel = document.createElement("div"),       
+                icon = document.createElement("img"),          
+                name = document.createElement("div"),        
+                remLink = document.createElement("a"),
+                remPic = document.createElement("img"),    
+                windowMain = document.createElement("div"), 
+                bottomLabel = document.createElement("div"),         
+                loadIcon = document.createElement("img"),         
+                bottomLabelText = document.createElement("div"); 
+            
+            windowInst.setAttribute("id", this.windowId);
+            windowInst.classList.add("Window");    
+            windowInst.setAttribute("title", "Window");
+            windowInst.style.zIndex = Projekt.zIndex;    
+            document.getElementById("content").appendChild(windowInst);
+    
+            topLabel.classList.add("topLabel");    
+        
+            icon.classList.add("labelIcon");
+            icon.setAttribute("src", _iconURL);    
+    
+            name.classList.add("appName");    
+            name.innerHTML = _name;
+    
+            remPic.classList.add("imgClose");
+            remPic.setAttribute("src", "css/pics/remove_16.png");    
+            remLink.setAttribute("title", "Close");
+            remLink.setAttribute("href", "#");
+    
+            windowMain.classList.add("windowMain");    
+            windowMain.setAttribute("title", "Main");
+            windowMain.setAttribute("id", this.windowMainId);
+    
+            bottomLabel.classList.add("bottomLabel");    
+            bottomLabel.setAttribute("title", "Status");
+    
+            loadIcon.classList.add("loadIcon");
+            loadIcon.setAttribute("id","loadIcon"+_instance);    
+            loadIcon.setAttribute("src", "css/pics/ajax-loader.gif");
+    
+            bottomLabelText.setAttribute("id","bottomLabelText"+_instance);   
+            bottomLabelText.setAttribute("title", "StatusText"); 
+            bottomLabelText.style.color = "White";
+            bottomLabelText.innerHTML = "Laddar, vänta...";
+            bottomLabel.appendChild(bottomLabelText);  
+    
+            windowInst.appendChild(topLabel);
+            topLabel.appendChild(icon);     
+            topLabel.appendChild(name);     
+            topLabel.appendChild(remLink);     
+            remLink.appendChild(remPic);    
+            windowInst.appendChild(windowMain);
+            bottomLabel.appendChild(loadIcon);     
+            windowInst.appendChild(bottomLabel);
+    
+            this.calculateWindowPosition(this.instanceId); 
+          
+            this.focusOrRemoveWindow = function(e){
+                // focus Window
+                var Window = null; 
+                if (e.target.getAttribute("title") === "Window")
+                    Window = e.target;
+                if (e.target.parentNode.getAttribute("title") === "Window")
+                    Window = e.target.parentNode;
+                if (e.target.parentNode.parentNode.getAttribute("title") === "Window")
+                    Window = e.target.parentNode.parentNode;
+    
+                if (Window){            
+                    document.getElementById(Window.getAttribute("id")).style.zIndex = ++Projekt.zIndex;            
+                }
+                // remove Window 
+                if (e.target.parentNode.getAttribute("title") === "Close"){            
+                    var node = e.target.parentNode.parentNode.parentNode;  
+                    node.parentNode.removeChild (node);
+                };
+            }; 
+            
+            windowInst.addEventListener("click", this.focusOrRemoveWindow);          
+        };
     };
-};
-return Window;
+    return Window;
 });
